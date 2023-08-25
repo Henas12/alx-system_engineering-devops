d2dae5b016f27144480449124d7609d80040dc66
Mikael394 <72981523+Mikael394@users.noreply.github.com>
2023-08-25 15:08:06 -0700

    Trying

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# Creates a file
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
+# Create a file using puppet
+file { '/tmp/holberton':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
